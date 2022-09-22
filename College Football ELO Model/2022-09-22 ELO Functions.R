ed0605e8ff9128895eb3b4680327fdf669ce0e38
Alex Elfering <alex.elfering@gmail.com>
2022-09-22 11:31:30 -0500

    added cfb elo functions
    the eventual goal is to break the long code of the current model into bite-sizeable chunks for better testing

---
+++ College Football ELO Model/2022-09-22 ELO Functions.R
@@ -0,0 +1,75 @@
+# ELO functions
+
+change_school_names <- function(df, column_name, new_name){
+  
+  column_name = enquo(column_name)
+  
+  df_new <- df %>%
+    mutate(new_name = case_when(!!column_name == 'UTEP' ~ 'Texas-El Paso',
+                                !!column_name == 'UAB' ~ 'Alabama-Birmingham',
+                                !!column_name == 'BYU' ~ "Brigham Young",
+                                !!column_name == 'UCF' ~ "Central Florida",
+                                !!column_name == "LSU" ~ "Louisiana State",
+                                !!column_name == "Ole Miss" ~ "Mississippi",
+                                !!column_name == "Pitt" ~ "Pittsburgh",
+                                !!column_name == "USC" ~ 'Southern California',
+                                !!column_name == 'SMU' ~ "Southern Methodist",
+                                !!column_name == 'UTSA' ~ 'Texas-San Antonio',
+                                TRUE ~ !!column_name))
+  
+  return(df_new)
+  
+}
+
+elo_week_update <- function(df, week_int = i, team_adv = 75, opp_adv = -75, k_val = 30){
+  
+  week_season <- df %>%
+    filter(wk == i) %>%
+    #filter(School == 'Nebraska') %>%
+    left_join(use_elo_df,
+              by = c('school' = 'school')) %>%
+    rename(school_elo = rating) %>%
+    left_join(use_elo_df,
+              by = c('opponent' = 'school')) %>%
+    rename(opp_elo = rating) %>%
+    mutate(school_elo_adj = case_when(location == '' ~ school_elo + team_adv,
+                                      location == '@' ~ school_elo + (team_adv*-1),
+                                      location == 'N' ~ school_elo + 0)) %>%
+    mutate(opp_elo_adj = case_when(location == '@' ~ opp_elo + team_adv,
+                                   location == '' ~ opp_elo + (team_adv*-1),
+                                   location == 'N' ~ opp_elo + 0)) %>%
+    mutate(m = (school_elo_adj - opp_elo_adj)/400,
+           elo_diff = abs(school_elo_adj-opp_elo_adj),
+           ps = round(elo_diff/25, 1),
+           p_opponent = 1/(1+10^m),
+           p_team = 1-p_opponent,
+           margin_abs = abs(pts-opp),
+           mov_mult = log(margin_abs + 1)*(2.2/(elo_diff * 0.001 + 2.2)),
+           k_adj = k_val*log(margin_abs + 1),
+           elo_adj = (k_val * mov_mult) * (1-p_team),
+           team_elo_update = case_when(wins == 1 ~ school_elo + elo_adj,
+                                       ties == 1 ~ school_elo,
+                                       loses == 1 ~ school_elo + (elo_adj*-1) ),
+           opponent_elo_update = case_when(loses == 1 ~ opp_elo + elo_adj,
+                                           ties == 1 ~ opp_elo,
+                                           wins == 1 ~ opp_elo + (elo_adj * -1) )) %>%
+    select(season,
+           wk,
+           month,
+           day,
+           year,
+           school,
+           opponent,
+           location,
+           pts,
+           opp,
+           ps,
+           elo_adj,
+           p_team,
+           p_opponent,
+           school_elo = team_elo_update,
+           opponent_elo = opponent_elo_update)
+  
+}
+
+test_update <- elo_week_update(df = use_season)
