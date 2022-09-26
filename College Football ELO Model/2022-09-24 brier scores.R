42ec2c129f4ace9fd0c3920419676b2cde26b2b5
Alex Elfering <alex.elfering@gmail.com>
2022-09-26 11:19:38 -0500

    updated brier score logic

--- College Football ELO Model/2022-09-24 brier scores.R
+++ College Football ELO Model/2022-09-24 brier scores.R
@@ -6,20 +6,37 @@ library(glue)
 db1b <- list.files('~/GitHub/Sports/College Football ELO Model/Test Data', pattern = "*.csv", full.names = TRUE)
 db1b_DF <- as.data.frame(rbindlist(lapply(db1b, fread)))
 
-db1b_DF %>%
+bracket_brier <- db1b_DF %>%
+  mutate(brier = ifelse(wins == 1, brier, NA)) %>%
   separate(group, into = c('lower', 'upper'), sep = ',') %>%
   mutate(lower = as.numeric(gsub('\\(', '', lower)),
-         upper = as.numeric(gsub('\\]', '', upper)),
+         upper = as.numeric(gsub('\\]', '', upper))) %>%
+  group_by(test_no,
+           season,
+           lower,
+           upper,
+           k_val,
+           home_adv,
+           regress_val) %>%
+  summarise(wins = sum(wins),
+            games = sum(game),
+            median_brier = median(brier, na.rm = TRUE),
+            mean_brier = mean(brier, na.rm = TRUE)) %>%
+  ungroup() %>%
+  mutate(pct = wins/games,
          median = (upper+lower)/2, 
-         distance = abs(pct-median)) %>%
+         distance = abs(pct-median))
+
+bracket_brier %>%
   group_by(test_no,
            k_val,
            home_adv,
            regress_val) %>%
-  summarise(#median_dist = round(median(distance),4),
-            #mean_dist = round(mean(distance),4),
-            median_brier = round(median(median_brier),4),
-            mean_brier = round(mean(mean_brier),4)) %>%
+  summarise(median_brier = median(median_brier, na.rm = TRUE),
+            mean_brier = mean(mean_brier, na.rm = TRUE),
+            median_dist = median(distance, na.rm = TRUE),
+            mean_dist = mean(distance, na.rm = TRUE)) %>%
   ungroup() %>%
   arrange(median_brier) %>%
+  filter(row_number() <= 10) %>%
   as.data.frame()
