a49b6cd76700bdf26c2795f371b570602bd9976f
Alex Elfering <alex.elfering@gmail.com>
2022-10-23 05:53:59 -0500

    added change in bowl odds for each fbs

--- College Football Bowl Predictions/2022-10-22 game prediction tables.R
+++ College Football Bowl Predictions/2022-10-22 game prediction tables.R
@@ -25,12 +25,15 @@ percent_100 <- function(.x) {
 max_iter_wk <- max(weekly_forecast$wk)
 
 latest_forecast <- weekly_forecast %>%
-  filter(wk == max_iter_wk) %>%
-  arrange(desc(bowl_times),
-          desc(runs_table)) %>%
   mutate(bowl_times = round((bowl_times/n_times)*100,3),
          win_out_times = round((win_out_times/n_times)*100,3),
          runs_table = round((runs_table/n_times)*100,3)) %>%
+  group_by(school) %>%
+  arrange(wk) %>%
+  mutate(change_bowl = bowl_times-lag(bowl_times)) %>%
+  filter(wk == max_iter_wk) %>%
+  arrange(desc(bowl_times),
+          desc(runs_table)) %>%
   unite(current_record, c('c_wins', 'c_lose'), sep = '-') %>%
   unite(predicted_record, c('avg_wins', 'avg_loses'), sep = '-')
 
