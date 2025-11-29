fe86e3136b05966e26a8d29605de9275edfee201
Alex <109438131+alex-elfering@users.noreply.github.com>
2025-11-29 07:59:53 -0600

    added school colors dataframe

---
+++ College Cross Country/20251118 MW Regionals/20251129 school colors.R
@@ -0,0 +1,72 @@
+school_colors <- data.frame(
+  school = c('Oklahoma State',
+             'Iowa State',
+             'Wichita State',
+             'Oklahoma',
+             'Bradley',
+             'South Dakota State',
+             'Oral Roberts',
+             'Mizzou',
+             'Tulsa',
+             'Kansas',
+             'Ill-Chicago',
+             'Loyola-Ill',
+             'Minnesota',
+             'Drake',
+             'North Dakota',
+             'Illinois',
+             'North Dakota State',
+             'Ill State',
+             'Nebraska',
+             'Iowa',
+             'Kansas State',
+             'DePaul',
+             'Eastern Illinois',
+             'SE Missouri',
+             'St. Thomas',
+             'Creighton',
+             'Saint Louis',
+             'South Dakota',
+             'Lindenwood',
+             'Southern Illinois',
+             'Western Illinois',
+             'Northern Iowa',
+             'Chicago State',
+             'SIU Edwardsville'),
+  primary_color = c(
+    '#FF7300',  # Oklahoma State - Orange
+    '#C8102E',  # Iowa State - Cardinal
+    '#FFCD00',  # Wichita State - Yellow
+    '#841617',  # Oklahoma - Crimson
+    '#C8102E',  # Bradley - Red
+    '#0033A0',  # South Dakota State - Blue
+    '#003594',  # Oral Roberts - Blue
+    '#F1B82D',  # Mizzou - Gold
+    '#003DA5',  # Tulsa - Blue
+    '#0051BA',  # Kansas - Blue
+    '#001E62',  # UIC - Navy
+    '#8A1538',  # Loyola - Maroon
+    '#7A0019',  # Minnesota - Maroon
+    '#0033A0',  # Drake - Blue
+    '#009A44',  # North Dakota - Green
+    '#13294B',  # Illinois - Navy/Orange (Navy primary)
+    '#006633',  # North Dakota State - Green
+    '#CE1126',  # Illinois State - Red
+    '#E41C38',  # Nebraska - Red
+    '#FFCD00',  # Iowa - Gold (use with black)
+    '#512888',  # Kansas State - Purple
+    '#004B87',  # DePaul - Blue
+    '#004B83',  # Eastern Illinois - Blue
+    '#C8102E',  # SE Missouri - Red
+    '#512888',  # St. Thomas - Purple
+    '#005CA9',  # Creighton - Blue
+    '#005DAA',  # Saint Louis - Blue
+    '#D6001C',  # South Dakota - Red
+    '#B5A36A',  # Lindenwood - Black/Gold
+    '#5D1725',  # Southern Illinois - Maroon
+    '#6A287E',  # Western Illinois - Purple
+    '#4B116F',  # Northern Iowa - Purple
+    '#00563F',  # Chicago State - Green
+    '#CE1141'   # SIU Edwardsville - Red
+  )
+)
